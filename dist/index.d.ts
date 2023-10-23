4569bd723cb0693b9c03efa3ed8023ad0c51ab3b
jean.luc.verhanneman@profectus-it.be <maze014.dev.prod@gmail.com>
2023-10-24 01:54:58 +0200

    tweaks

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { ActivationOptionType, getActivatedOptions } from "./optionActivations";
+import { Checker } from "./optionCombinatorChecker";
+export { ActivationOptionType, getActivatedOptions, Checker };
